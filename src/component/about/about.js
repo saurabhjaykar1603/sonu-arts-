e8c44e026413ebd499c5dc847a6037572d90a7b0
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-31 20:02:17 +0530

    docs - file added, navbar-created, navbar-linked

---
+++ src/component/about/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './about.css'
+import Navbar from '../navbar/navbar'
+
+function About() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default About
---
+++ src/component/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+import Navbar from '../navbar/navbar'
+
+function Home() {
+  return (
+    <div>
+   <Navbar/>
+    </div>
+  )
+}
+
+export default Home
